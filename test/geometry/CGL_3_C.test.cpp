ecbee422df3e3e6dd8f73a0da32ba972147e33a4
Hisato Matsukawa <s1260165@std2dc6.u-aizu.ac.jp>
2019-10-21 20:34:37 +0900

    u

--- test/geometry/CGL_3_C.test.cpp
+++ test/geometry/CGL_3_C.test.cpp
@@ -18,29 +18,23 @@ template<typename T>istream & operator >> (istream &i,vector<T> &A){for(auto &I:
 template<typename T,typename U>ostream & operator << (ostream &o,const pair<T,U> &A){o<<A.F<<" "<<A.S; return o;}
 template<typename T>ostream & operator << (ostream &o,const vector<T> &A){ll i=A.size(); for(auto &I:A){o<<I<<(--i?" ":"");} return o;}
 
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_3_B"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_3_C"
 //#define ERROR "0.00000001"
 
 
 int main(){
     cout<<fixed<<setprecision(12);
+    ll n;
+    cin>>n;
+    vector<P> A(n);
+    cin>>A;
     ll q;
     cin>>q;
-    ll n=q;
-    vector<P> poly;
     while(q--){
-        D x1,y1;
-        cin>>x1>>y1;
-        P a={x1,y1};
-        poly.push_back(a);
+        P X;
+        cin>>X;
+        cout<<contain(A,X)+1<<endl;
     }
-    poly.push_back(P(poly[0]));
-    poly.push_back(P(poly[1]));
-    bool jd=true;
-    for(int i=1;i+1<(int)poly.size();i++){
-      jd&=cross(poly[i]-poly[i-1],poly[i+1]-poly[i])>EPS;
-    }
-    cout<<jd<<endl;
     
     return 0;
 }
